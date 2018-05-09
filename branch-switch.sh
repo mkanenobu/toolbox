3fa86adc4033bf1f99bf6fde8aff34e14b5126e4
mkanenobu <kaneda.1018@gmail.com>
2018-05-09 15:16:32 +0900

    05/09/18

--- branch-switch.sh
+++ branch-switch.sh
@@ -6,9 +6,10 @@ if [ "$1" = "show" ]; then
   :
 fi
 
-git fetch || exit 1
 target_branch="$(git branch -a | peco)"
-if [ "$(echo "$target_branch" | cut -d '/' -f 1)" = "  remotes" ]; then
+if [ "$(echo "$target_branch" | grep "\*")" ]; then
+  echo "It is current branch"
+elif [ "$(echo "$target_branch" | cut -d '/' -f 1)" = "  remotes" ]; then
   git checkout -t "$(echo "$target_branch" | sed -e "s/\ \ remotes\///")"
 else
   git checkout "$(echo "$target_branch" | sed -e "s/\ \ //")"
