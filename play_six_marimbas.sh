adac65f27ff7b604a6579f30b2c67e1e569ff8b5
mkanenobu <kaneda.1018@gmail.com>
2018-05-14 09:26:35 +0900

    05/14/18

--- play_six_marimbas.sh
+++ play_six_marimbas.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-while : ;do
-    mpv "$HOME/Music/Reich, Steve - Sextet - Six Marimbas/06 - Reich, Steve - Six Marimbas.opus" || break
-done
+if [ "$#" -eq 0 ]; then
+    while : ; do
+        mpv "$HOME/Music/Reich, Steve - Sextet - Six Marimbas/06 - Reich, Steve - Six Marimbas.opus" || break
+    done
+elif [ "$#" -eq 1 ]; then
+    while : ; do
+        mpv "$1" || break
+    done
+fi
+
