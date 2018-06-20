3ac85b09d191e0c8993949b6d59e2dc14038aa77
mkanenobu <kaneda.1018@gmail.com>
2018-06-20 17:05:25 +0900

    add search available fonts script

---
+++ available_fonts.nim
@@ -0,0 +1,14 @@
+import sequtils, strutils, osproc, algorithm, sets
+
+var installedFonts:seq[string] = execProcess("/usr/bin/fc-list").splitLines
+# remove blank line
+discard installedFonts.pop
+
+for i, font in installedFonts:
+  installedFonts[i] = font.split(':')[1]
+
+installedFonts = deduplicate(installedFonts)
+installedFonts.sort(cmp)
+
+for font in installedFonts:
+  echo font.strip
