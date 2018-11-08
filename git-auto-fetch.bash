5b21f27afd577d728f1a76711783f8b7a94e7659
mkanenobu <kaneda.1018@gmail.com>
2018-11-08 09:36:31 +0900

    11/08/18

--- git-auto-fetch.bash
+++ git-auto-fetch.bash
@@ -1,46 +1,33 @@
 #!/bin/bash
 git-auto-fetch() {
-  hists_file="${HOME}/.cache/.git-auto-fetch"
-  tmp_file="${HOME}/.cache/.git-auto-fetch-tmp"
-
-  if [ ! "$(git root 2>/dev/null)" ]; then
+  current_root="$(git root 2>/dev/null)"
+  if [ -z "${current_root}" ]; then
     return
   fi
 
   current_time="$(date +%s)"
-  current_root="$(git root)"
-  interval="600" # second
-  exists_flg=0
-  fetch_flg=0
+  interval="600" # seconds
+
+  hists_file="${HOME}/.cache/.git-auto-fetch"
+  tmp_file="${HOME}/.cache/.git-auto-fetch-tmp"
 
   if [ -f "${hists_file}" ]; then
-    cat "${hists_file}" >| "${tmp_file}"
-    while read -r line; do
-      saved_time="$(echo "${line}" | cut -d"," -f 1)"
-      saved_root="$(echo "${line}" | cut -d"," -f 2)"
-      if [ "${saved_root}" = "${current_root}" ]; then
-        exists_flg=1
-        if [ "$((current_time - saved_time))" -gt "${interval}" ]; then
-          fetch_flg=1
-        fi
+    saved="$(rg -N "${current_root}" "${hists_file}")"
+    if [ -z "${saved}" ]; then
+      cat "${hists_file}" >| "${tmp_file}"
+      echo "${current_time},${current_root}" >> "${tmp_file}"
+    else
+      saved_time="$(echo "${saved}" | cut -d"," -f 1)"
+      if [ "$((current_time - saved_time))" -gt "${interval}" ]; then
+        (git fetch >/dev/null &)
+        cat "${hists_file}" >| "${tmp_file}"
+        echo "${current_time},${current_root}" >> "${tmp_file}"
       fi
-    done < "${hists_file}"
+    fi
   fi
 
-  if [ "${exists_flg}" -eq 0 ]; then
-    echo "${current_time},${current_root}" >>"${tmp_file}"
-  fi
-
-  if [ "$fetch_flg" -eq 1 ]; then
-    (git fetch >/dev/null &)
-    echo "${current_time},${current_root}" >>"${tmp_file}"
-  fi
-
-  if [ -s "${tmp_file}" ]; then
+  if [ -f "${tmp_file}" ]; then
     cat "${tmp_file}" >| "${hists_file}"
     rm "${tmp_file}"
   fi
-
-  exists_flg=0
-  fetch_flg=0
 }
