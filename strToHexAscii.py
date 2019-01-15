8314fcbb588d68457e2e8b1a9736f423adfde9b7
mkanenobu <kaneda.1018@gmail.com>
2019-01-15 08:25:53 +0000

    01/15/19

--- strToHexAscii.py
+++ strToHexAscii.py
@@ -4,7 +4,14 @@ import sys
 
 args = sys.argv
 
-for arg in args[1]:
+s = ""
+
+if len(args) == 1:
+    s = input("Please input:")
+else:
+    s = args[1]
+
+for arg in s:
     for i in arg:
         print(format(ord(i), 'x').upper(), end=" ")
 
