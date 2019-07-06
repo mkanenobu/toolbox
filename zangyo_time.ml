a922351efb50ab2c79f4dc87d7a3fc5e74a76bcf
mkanenobu <kaneda.1018@gmail.com>
2019-07-06 21:10:12 +0900

    Sat Jul  6 21:10:12 JST 2019

--- zangyo_time.ml
+++ zangyo_time.ml
@@ -1,5 +1,6 @@
 open Printf
 
+
 let seconds_from_start_of_today (hour, minute) =
   hour * 3600 + minute * 60
 
@@ -14,8 +15,14 @@ let now =
   let now = Unix.localtime @@ Unix.time () in
   now.tm_hour, now.tm_min
 
-let syukkin =
-  let input = int_of_string Sys.argv.(1) in
+(* TODO: APIで始業時間を取得 *)
+let syukkin str =
+  let pattern = Str.regexp "[0-9][0-9][0-9][0-9]" in
+  if not (Str.string_match pattern str 0) then (
+    printf "Invalid format\n";
+    exit 2
+  );
+  let input = int_of_string str in
   input / 100, input mod 100
 
 let working_seconds =
@@ -23,7 +30,12 @@ let working_seconds =
 
 
 let () =
-  let syukkin_time = seconds_from_start_of_today syukkin in
+  if Array.length Sys.argv <= 1 then (
+    printf "No argument\n";
+    printf "COMMAND [4digits]\n";
+    exit 1
+  );
+  let syukkin_time = seconds_from_start_of_today @@ syukkin Sys.argv.(1) in
   let now_time = seconds_from_start_of_today now in
   printf "%s\n" @@ string_of_seconds (now_time - syukkin_time - working_seconds)
 
