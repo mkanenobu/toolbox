f5051809ebf32b6bda5a5d7d982015aa8db5de6e
Mitsuru Kanenobu <mitsuru.kanenobu@integrity-healthcare.co.jp>
2019-07-22 19:53:33 +0900

    Mon Jul 22 19:53:33 JST 2019

--- rgb_to_hex.ml
+++ rgb_to_hex.ml
@@ -3,11 +3,15 @@ open Core
 let to_hex rgb_code =
   sprintf "%X" rgb_code
 
+let preprocess s =
+  let pattern = Str.regexp @@ Str.quote "," in
+  Str.global_replace pattern "" s
+
 let () =
   if Array.length Sys.argv <> 4 then exit 1;
   let args =
     Array.filteri Sys.argv ~f:(fun i _ -> i <> 0) |>
-    Array.map ~f:(fun e -> int_of_string e)
+    Array.map ~f:(fun e -> int_of_string (preprocess e))
   in
   let result = Buffer.create 7 in
   Buffer.add_char result '#';
