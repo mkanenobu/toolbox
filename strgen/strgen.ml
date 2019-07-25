1545ce9189cffd97efeea041a98c9a1fce1dc754
mkanenobu <kaneda.1018@gmail.com>
2019-07-25 09:19:13 +0900

    Thu Jul 25 09:19:13 JST 2019

--- strgen/strgen.ml
+++ strgen/strgen.ml
@@ -4,8 +4,11 @@ let parse_args args =
   int_of_string args.(1)
 
 let () =
+  let len = if Array.length Sys.argv > 1 then int_of_string (Sys.argv.(1)) else 8 in
   let result = Buffer.create 0 in
-  for i = 0 to 100 do
-    Buffer.add_char result 'a';
+  Random.self_init ();
+  for i = 0 to (len - 1) do
+    let c = char_of_int (Random.int 100) in
+    Buffer.add_char result c;
   done;
   print_endline @@ Buffer.contents result;
