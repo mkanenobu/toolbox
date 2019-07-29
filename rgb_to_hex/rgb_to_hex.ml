eaa21f16632ad4033d5cf3c29a16a47eb244ec63
Mitsuru Kanenobu <mitsuru.kanenobu@integrity-healthcare.co.jp>
2019-07-29 19:25:37 +0900

    Mon Jul 29 19:25:37 JST 2019

--- rgb_to_hex/rgb_to_hex.ml
+++ rgb_to_hex/rgb_to_hex.ml
@@ -5,37 +5,47 @@ let exit_with_message exit_code message =
   print_endline message;
   exit exit_code
 
+let preprocess s =
+  let pattern = Str.regexp "\\(,\\|\\ \\|(\\|)\\)" in
+  let remove pattern = Str.global_replace pattern "" in
+  s |> remove pattern
+
+let parse_one_arg arg =
+  try
+    Some (String.split arg ~on:(' ') |> List.map ~f:(fun e -> preprocess e))
+  with _ -> None
+
+let int_list_of_string_list l =
+  try
+    List.map l ~f:(fun e -> int_of_string (preprocess e))
+  with _ -> exit_with_message 3 "Argument is not number"
+
 let to_hex rgb_code =
   sprintf "%02X" rgb_code
 
-let preprocess s =
-  let comma = Str.regexp @@ Str.quote "," in
-  let parenthetis_open = Str.regexp @@ Str.quote "(" in
-  let parenthetis_end = Str.regexp @@ Str.quote ")" in
-  let whitespace  = Str.regexp @@ Str.quote " " in
-  let remove pattern = Str.global_replace pattern "" in
-  s |> remove comma |> remove parenthetis_open |> remove parenthetis_end |> remove whitespace
-
 let validate_args args =
-  Array.iter args ~f:(fun arg ->
+  List.iter args ~f:(fun arg ->
       if arg < 0 || arg > 255 then exit_with_message 2 "Value is out of range (0 <= n <= 255)"
     )
 
 let () =
-  if Array.length Sys.argv <> 4
-  then exit_with_message 1 (sprintf "rgb-to-hex takes 3 args (%d args given)" @@ Array.length Sys.argv - 1);
-
-  let args =
-    try
-      Array.filteri Sys.argv ~f:(fun i _ -> i <> 0) |>
-      Array.map ~f:(fun e -> int_of_string (preprocess e))
-    with _ -> exit_with_message 3 "Argument is not number"
+  let raw_args = Array.to_list Sys.argv |> List.tl_exn in
+  let arglen = Array.length Sys.argv - 1 in
+  let args = match arglen with
+    | 1 -> (
+        match parse_one_arg @@ List.nth_exn raw_args 0 with
+        | Some l_ -> int_list_of_string_list l_
+        | _ -> exit_with_message 1 @@ sprintf "rgb-to-hex takes 3 args (%d args given)" arglen
+      )
+    | 3 -> int_list_of_string_list raw_args
+    | _ -> exit_with_message 1 @@ sprintf "rgb-to-hex takes 3 args (%d args given)" arglen
   in
+
   validate_args args;
 
   let result = Buffer.create 7 in
   Buffer.add_char result '#';
-  Array.iter args ~f:(fun e -> Buffer.add_string result @@ to_hex e);
+  List.iter args ~f:(fun e -> Buffer.add_string result @@ to_hex e);
   print_endline @@ Buffer.contents result;
   match Hashtbl.find color_names (Buffer.contents result) with
   | Some s -> print_endline s;
