ccfb679c276ab1cb535b99e6e4254401a817853a
Mitsuru Kanenobu <mitsuru.kanenobu@integrity-healthcare.co.jp>
2019-09-03 18:13:03 +0900

    Tue Sep  3 18:13:03 JST 2019

--- ggr.py
+++ ggr.py
@@ -5,7 +5,7 @@ import sys
 import urllib.parse as urlparse
 import webbrowser as browser
 
-search_string = sys.argv[1]
+search_string = ' '.join(sys.argv[1:])
 query_strings = {
     "google": "https://www.google.com/search?q=%s",
     "google-us": "https://www.google.com/search?gl=us&hl=en&q=%s",
