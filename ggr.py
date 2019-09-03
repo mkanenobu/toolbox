94675e6b15f5372aced9a72a8d7a6d824feaac22
Mitsuru Kanenobu <mitsuru.kanenobu@integrity-healthcare.co.jp>
2019-09-03 18:10:17 +0900

    Tue Sep  3 18:10:17 JST 2019

--- ggr.py
+++ ggr.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import optparse
 import sys
 import urllib.parse as urlparse
 import webbrowser as browser
 
 search_string = sys.argv[1]
 query_strings = {
-        "google": "https://www.google.com/search?q=%s",
-        }
+    "google": "https://www.google.com/search?q=%s",
+    "google-us": "https://www.google.com/search?gl=us&hl=en&q=%s",
+}
+
 
 def build_query(query_string, search_string):
     return query_string % (urlparse.quote(search_string))
+
 
 browser.open_new_tab(build_query(query_strings["google"], search_string))
