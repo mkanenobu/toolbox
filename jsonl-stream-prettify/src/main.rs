72818ca8d615561a05b0ab51dd09e726fcb0da86
mkanenobu <mitsuru.kanenobu@gmail.com>
2024-12-09 19:46:57 +0900

    Mon Dec  9 19:46:57 JST 2024

--- jsonl-stream-prettify/src/main.rs
+++ jsonl-stream-prettify/src/main.rs
@@ -1,6 +1,6 @@
-use std::io::{self, BufRead};
-use sonic_rs;
 use colored_json::prelude::*;
+use sonic_rs;
+use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
@@ -9,14 +9,17 @@ fn main() {
     for line in handle.lines() {
         match line {
             Ok(log_line) => {
-                match sonic_rs::from_str::<sonic_rs::Value>(&log_line) {
-                    Ok(json_value) => {
-                        match sonic_rs::to_string_pretty(&json_value) {
-                            Ok(pretty_json) => println!("{}", pretty_json.to_colored_json_auto().unwrap()),
-                            Err(err) => println!("Failed to pretty print JSON: {}", err),
-                        }
-                    }
-                    Err(_) => println!("{}", &log_line),
+                if let Ok(json_value) = sonic_rs::from_str::<sonic_rs::Value>(&log_line) {
+                    sonic_rs::to_string_pretty(&json_value)
+                        .map(|pretty_json| {
+                            println!(
+                                "{}",
+                                pretty_json.to_colored_json_auto().unwrap_or(pretty_json)
+                            )
+                        })
+                        .unwrap_or_else(|err| println!("Failed to pretty print JSON: {}", err));
+                } else {
+                    println!("{}", &log_line)
                 }
             }
             Err(e) => eprintln!("Error reading line: {}", e),
