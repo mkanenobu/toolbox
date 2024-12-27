1c43536b24a266aa159cf9fe593bcb7eda4670f9
mkanenobu <mitsuru.kanenobu@gmail.com>
2024-12-27 16:07:02 +0900

    Fri Dec 27 16:07:02 JST 2024

--- colortest.js
+++ colortest.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
-const TEXT = 'gYw';
+const TEXT = "gYw";
 
 const foregrounds = [
-  'm', '1m', '30m', '1;30m', '31m', '1;31m', '32m',
-  '1;32m', '33m', '1;33m', '34m', '1;34m', '35m', '1;35m',
-  '36m', '1;36m', '37m', '1;37m'
+  "m", "1m", "30m", "1;30m", "31m", "1;31m", "32m",
+  "1;32m", "33m", "1;33m", "34m", "1;34m", "35m", "1;35m",
+  "36m", "1;36m", "37m", "1;37m"
 ];
 
-const backgrounds = [
-    '40m', '41m', '42m', '43m', '44m', '45m', '46m', '47m'
-];
+const backgrounds = ["40m", "41m", "42m", "43m", "44m", "45m", "46m", "47m"];
 
 process.stdout.write(`\n                 ${backgrounds.join("     ")}\n`);
 
